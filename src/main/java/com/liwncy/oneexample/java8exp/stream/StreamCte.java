526732fc305870e35046fd7129a998973e25e0f1
Li Wanchang <wanchang.li@pactera.com>
2020-09-28 15:51:19 +0800

    up

--- src/main/java/com/liwncy/oneexample/java8exp/stream/StreamCte.java
+++ src/main/java/com/liwncy/oneexample/java8exp/stream/StreamCte.java
@@ -7,10 +7,6 @@ import java.math.BigDecimal;
 import java.util.*;
 import java.util.stream.Collectors;
 
-import static java.util.Comparator.comparingLong;
-import static java.util.stream.Collectors.collectingAndThen;
-import static java.util.stream.Collectors.toCollection;
-
 /**
  * Stream测试例子
  * Java8 快速实现List转map 、分组、过滤等操作
@@ -24,6 +20,7 @@ public class StreamCte {
         // 1,分组
         Map<Integer, List<Apple>> groupBy = aList.stream().collect(Collectors.groupingBy(Apple::getId));
         System.out.println("Group:" + groupBy);
+
         // 2,List转Map
         /**
          * List -> Map
@@ -34,32 +31,39 @@ public class StreamCte {
          */
         Map<Integer, Apple> aMap = aList.stream().collect(Collectors.toMap(Apple::getId, a -> a, (k1, k2) -> k1));
         System.out.println("Map:" + aMap);
+
         // 3,过滤Filter
         List<Apple> fList = aList.stream().filter(a -> a.getId() == 1).collect(Collectors.toList());
         System.out.println("Filtered:" + fList);
+
         // 4,求和
         BigDecimal totalMoney = aList.stream().map(Apple::getMoney).reduce(BigDecimal.ZERO, BigDecimal::add);
         Integer totalId = aList.stream().map(Apple::getId).reduce(0, Integer::sum);
         System.out.println("total:" + totalMoney);
         System.out.println("total:" + totalId);
+
         // 5,查找流中最大 最小值
         Optional<Apple> maxA = aList.stream().collect(Collectors.maxBy(Comparator.comparing(Apple::getId)));
         maxA.ifPresent(System.out::println);
-
-        Optional<Apple> minA = aList.stream().
-                collect(Collectors.minBy(Comparator.comparing(Apple::getName)));
+        Optional<Apple> minA = aList.stream().collect(Collectors.minBy(Comparator.comparing(Apple::getName)));
         minA.ifPresent(System.out::println);
+
         // 6,去重
-        //// 根据字段去重
+        //// 6.1 根据字段去重
         List<Apple> unique = aList.stream().collect(
-                collectingAndThen(toCollection(() -> new TreeSet<>(comparingLong(Apple::getId))), ArrayList::new)
+                Collectors.collectingAndThen(Collectors.toCollection(() -> new TreeSet<>(Comparator.comparingLong(Apple::getId))), ArrayList::new)
+        );
+        //// 6.2 根据两个字段组合去重
+        List<Apple> unique0 = aList.stream().collect(
+                Collectors.collectingAndThen(Collectors.toCollection(() -> new TreeSet<>(Comparator.comparing(apple -> apple.getId()+ apple.getName()))), ArrayList::new)
         );
         System.out.println(unique);
-        //// distinct去除相同数据()
+        System.out.println(unique0);
+        //// 6.3 distinct只能去除相同数据()
         aList.add(new Apple(3, "荔枝", new BigDecimal("9.99"), 40));
         List<Apple> unique2 = aList.stream().distinct().collect(Collectors.toList());
         System.out.println(unique2);
-        //// 根据字段去重(filter实现)
+        //// 6.4 根据字段去重(filter实现)
         List<Integer> ids = new ArrayList<>();
         List<Apple> unique3 = aList.stream().filter(
             v -> {
@@ -69,6 +73,9 @@ public class StreamCte {
             }
         ).collect(Collectors.toList());
         System.out.println(unique3);
+        //// 6.5 根据map转List实现字段去重(可多字段)
+        List<Apple> unique4 = new ArrayList<>(aMap.values());
+        System.out.println("unique4:"+unique4);
     }
 
     public static List<Apple> getList() {
